3a5d67385ed384b12d58ad132ae69a58f1818052
Richard Albert Leddy <858872+rleddy@users.noreply.github.com>
2023-11-17 20:35:08 -0800

    Update index.js
    Rethink just a few points about locks

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ const NUM_INFO_FIELDS = 4
 
 const LRU_HEADER = 64
 
+const DEFAULT_ELEMENT_COUNT = 1024
+
+const DEFAULT_SET_ATTEMPTS = 10
+const DEFAULT_GET_ATTEMPTS = 10
+const DEFAULT_DEL_ATTEMPTS = 10
+
 
 var g_app_seed = 0
 var g_hasher32 = null
@@ -50,7 +56,11 @@ function init_default(seed) {
 }
 
 
-
+/**
+ * In this module, the ReaderWriter represents on mutex fo lock operations, used to protect 
+ * data structure changes on one HopScotch table comprising two shared memory sections, the 
+ * storage region and the hash table.
+ */
 class ReaderWriter {
 
     //
@@ -84,7 +94,7 @@ class ReaderWriter {
                 resolve(true)
             } else if ( result === false ) {
                 if ( count < MAX_LOCK_ATTEMPTS ) {
-                    this.try_again(resolve,count)
+                    this.try_again(resolve,count)  // keep in mind that this not actually recursive
                 }
             } else {
                 reject(result)
@@ -92,20 +102,36 @@ class ReaderWriter {
         })
     }
 
+
+    /**
+     * Calls the posix mutex try lock. Hence, if the thread is locked this will return
+     * immediatly. If the mutex is locked, the lru module will return false. Otherwise, the
+     * asset will be locked, and it must be unlocked after access. 
+     * 
+     * On failure to lock, this method will relinquish the attemp to lock on the asset to the event queue
+     * by calling `try_again`.
+     */
     async access(count) {
         if ( count === undefined ) count = 0
-        return new Promise((resolve,reject) => {
-            this.asset_lock = false
-            let result = shm.try_lock(this.shm_com_key)
-            if ( result === true ) {
-                this.asset_lock = true
-                resolve(true)
-            } else if ( result === false ) {
-                this.try_again(resolve,reject,count)
+        return new Promise(async (resolve,reject) => {
+            if ( this.asset_lock ) {  // will await some number of counts to acquire the in this process (if this is reachable)
+                await this.try_again(resolve,reject,count)
             } else {
-                reject(result)
+                let result = shm.try_lock(this.shm_com_key)
+                if ( result === true ) {
+                    this.asset_lock = true    // avoid calling the POSIX methods until this is clear
+                    resolve(true)
+                } else if ( result === false ) {
+                    await this.try_again(resolve,reject,count)
+                } else {
+                    reject(result)
+                }    
             }
         })
+    }
+
+    async lock_asset_access(count) {
+        return await this.access(count)
     }
 
     lock_asset() {
@@ -124,23 +150,38 @@ class ReaderWriter {
             let result = shm.unlock(this.shm_com_key)
             if ( result !== true ) {
                 console.log(shm.get_last_mutex_reason(this.shm_com_key))
+            } else {
+                this.asset_lock = false
             }
         }
     }
     //
 }
 
-  // master_of_ceremonies -- a path
-  // proc_names -- the list of js file names that will be attaching to the regions.
-  // initializer -- true if master of ceremonies
-  //
-  // note: the initializer has to be called first before others.
+// master_of_ceremonies -- a path
+// proc_names -- the list of js file names that will be attaching to the regions.
+// initializer -- true if master of ceremonies
+//
+// note: the initializer has to be called first before others.
 
-
+/**
+ * 
+ * This class defines the relationship between the hash table and its use as an LRU. 
+ * This module accepts the applications hash of the data being used to identify the data.
+ * The hash must be 32 bits or less. Because, the size of the hash table is likely to be 
+ * smaller than the range of the hashes, this class augments the hash key by 
+ * taking the modulus of the hash with respect to the number of elements that the table will accomodate.
+ * 
+ * As long as the max table size remains fixed, the augmented hash will not have to change. 
+ * Objects may be updated against the old hash. If an application wishes to change the hash of an object to reflect
+ * the change of the data, the object should first be removed from the hash table and then be reinserted.
+ * 
+ */
 class ShmLRUCache extends ReaderWriter {
 
     constructor(conf) {
         super(conf)
+        this.count = DEFAULT_ELEMENT_COUNT
         this.conf = conf
         try {
             this.hasher = conf.hasher ? (() =>{ hasher = require(conf.hasher); return(hasher.init(conf.seed)); })()
@@ -150,6 +191,10 @@ class ShmLRUCache extends ReaderWriter {
         } catch (e) {
             this.hasher = init_default(conf.seed)
         }
+        //
+        this._allowed_set_attempts = DEFAULT_SET_ATTEMPTS
+        this._allowed_get_attempts = DEFAULT_GET_ATTEMPTS
+        this._allowed_del_attempts = DEFAULT_DEL_ATTEMPTS
         //
         this.eviction_interval = null
         this._use_immediate_eviction = false
@@ -163,18 +208,35 @@ class ShmLRUCache extends ReaderWriter {
     }
     
     
+    /**
+     * Does the intialization of the shared memory region from the perspective of the 
+     * calling process. 
+     * 
+     * The field `initializer` will determine which process creates the shared memory section and which one attach. 
+     * Just one process creates the section and for that process `initializer` is set to true. All others are set to false.
+     * The initializing process should be call before the others. This method does not attempt to make extra attempts to attach
+     * to memory and simply fails by throwing an exception.
+     * 
+     * Each process has the opportunity to identify itself in the `com_buffer` table, which is part of the shared memory region
+     * holding a mutex governing the use of the LRU tables. 
+     * 
+     * Every processes may call the method to intialize the mutex. The initialization method is called with the 
+     * field `initializer`, which is true for the creator and false for those attaching. (as before)
+     * 
+     * @param {object} conf 
+     */
     init_shm_communicator(conf) {
         //
         let sz = INTER_PROC_DESCRIPTOR_WORDS
         let proc_count = conf.proc_names ? conf.proc_names.length : 0
         //
         let mpath_match = -1
-        if ( (typeof conf.token_path !== "undefined") ) {
+        if ( (typeof conf.token_path !== "undefined") ) {  // better idea
             this.initializer = conf.am_initializer
             if ( this.initializer === undefined ) this.initializer = false
             proc_count = Math.max(proc_count,2)     // expect one attaching process other than initializer (May be just logging)
         } else {
-            mpath_match = conf.master_of_ceremonies.indexOf(conf.module_path)
+            mpath_match = conf.master_of_ceremonies.indexOf(conf.module_path)  // old idea
             this.initializer = ((proc_count > 0) && (mpath_match >= 0))
         }
         //
@@ -208,9 +270,14 @@ class ShmLRUCache extends ReaderWriter {
 
 
     // ----
+    /**
+     * 
+     * @param {object} conf 
+     */
     init_cache(conf) {
-        this.record_size = conf.record_size
-        this.count = conf.el_count
+        //
+        this.record_size = parseInt(conf.record_size)
+        this.count = (conf.el_count !== undefined) ? parseInt(conf.el_count) : DEFAULT_ELEMENT_COUNT;
         //
         if ( this.initializer ) {
             let sz = ((this.count*this.record_size) + LRU_HEADER)
@@ -254,6 +321,11 @@ class ShmLRUCache extends ReaderWriter {
         return( this.augment_hash(hh) )
     }
 
+    hash_pair(value) {
+        let hh = this.hasher(value)
+        return( this.augmented_hash_pair(hh) )
+    }
+
     // ----
     pure_hash(value) {
         let hh = this.hasher(value)
@@ -261,14 +333,52 @@ class ShmLRUCache extends ReaderWriter {
     }
 
     // ----
+    /**
+     * 
+     * The application determines the key, which should be a numeric hash of the value. 
+     * The hash of the value should be almost unique for smooth operation of the underlying hash table data structure.
+     * 
+     * This method returns an augmented hash, which is really the hash annotated with its
+     * modulus determined by the hash table size.
+     * 
+     * @param {Number} key 
+     * @param {object} value 
+     * @returns {string} - a hyphenated string where the left is the modulus of the hash and the right is the hash itself.
+     */
+
     augment_hash(hash) {
         let hh = hash
         let top = hh % this.count
-        let augmented_hash_token = top + '-' + hh
+        let augmented_hash_token = `${top}-${hh}`
         return( augmented_hash_token )
+    }
+
+    /**
+     * 
+     * The application determines the key, which should be a numeric hash of the value. 
+     * The hash of the value should be almost unique for smooth operation of the underlying hash table data structure.
+     * 
+     * This method returns an augmented hash, which is really the hash annotated with its
+     * modulus determined by the hash table size.
+     * 
+     * @param {Number} key 
+     * @param {object} value 
+     * @returns {Array} - a hyphenated string where a[0] is the modulus of the hash and a[1] is the hash itself.
+     */
+
+    augmented_hash_pair(hash) {
+        let hh = hash
+        let top = hh % this.count
+        return( [top,hh] )
     }
   
     // ----
+    /**
+     * 
+     * @param {string|Array} hash_augmented - hyphentated string or a pair
+     * @param {string|Number} value 
+     * @returns 
+     */
     async set(hash_augmented,value) {
         if ( typeof value === 'string' ) {
             if ( !(value.length) ) return(-1)
@@ -276,13 +386,19 @@ class ShmLRUCache extends ReaderWriter {
         } else {
             value = (value).toString(16)
         }
-        let pair = hash_augmented.split('-')
+        let pair = Array.isArray(hash_augmented) ?  hash_augmented : hash_augmented.split('-')
 		let hash = parseInt(pair[0])
 		let index = parseInt(pair[1])
         //
         if ( this.proc_index < 0 ) return  // guard against bad initialization and test cases
-        this.lock_asset()
-        let status = shm.set(this.lru_key,value,hash,index)
+        try {
+            await this.lock_asset_access(this._allowed_set_attempts)   // locks if possible
+        } catch (e) {
+            return false   // at this point the method could not gain access to lock; so, this method will not unlock
+        }
+        //
+        let status = shm.set(this.lru_key,value,hash,index)   // SET
+        //
         if ( ((status === false) || (status < 0)) && this._use_immediate_eviction) {
             // error condition...
             let time_shift = 0
@@ -303,28 +419,51 @@ class ShmLRUCache extends ReaderWriter {
         return status
     }
 
+    /**
+     * 
+     * @param {string} hash_augmented 
+     * @returns 
+     */
     async get(hash_augmented) {
-        let pair = hash_augmented.split('-')
+        let pair = Array.isArray(hash_augmented) ?  hash_augmented : hash_augmented.split('-')
 		let hash = parseInt(pair[0])
         let index = parseInt(pair[1])
         if ( this.proc_index < 0 ) return(false)  // guard against bad initialization and test cases
-        this.lock_asset()
+        try {
+            await this.lock_asset_access(this._allowed_get_attempts)   // locks if possible
+        } catch (e) {
+            return false // at this point the method could not gain access to lock; so, this method will not unlock
+        }
         let value = shm.get_el_hash(this.lru_key,hash,index)
         this.unlock_asset()
         return(value)
     }
 
+    /**
+     * 
+     * @param {string} hash_augmented 
+     * @returns 
+     */
     async del(hash_augmented) {
-        let pair = hash_augmented.split('-')
+        let pair = Array.isArray(hash_augmented) ?  hash_augmented : hash_augmented.split('-')
 		let hash = parseInt(pair[0])
         let index = parseInt(pair[1])
         if ( this.proc_index < 0 ) return(false)  // guard against bad initialization and test cases
-        this.lock_asset()
+        try {
+            await this.lock_asset_access(this._allowed_del_attempts)   // locks if possible
+        } catch (e) {
+            return false // at this point the method could not gain access to lock; so, this method will not unlock
+        }
         let result = shm.del_key(this.lru_key,hash,index)
         this.unlock_asset()
         return(result)
     }
 
+    /**
+     * 
+     * @param {string} hash_augmented 
+     * @returns 
+     */
     async delete(hash_augmented) {
         return this.del(hash_augmented)
     }
@@ -332,6 +471,13 @@ class ShmLRUCache extends ReaderWriter {
 
     // ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
+    /**
+     * 
+     * Called from within the `init_cache` method if the timeout is being used. 
+     * 
+     * @param {*} conf 
+     * @returns 
+     */
     setup_eviction_proc(conf) {
         let eviction_timeout = (conf.sessions_length !== undefined) ? conf.sessions_length : DEFAULT_RESIDENCY_TIMEOUT
         let prev_milisec = (conf.aged_out_secs !== undefined) ? (conf.aged_out_secs*1000) : DEFAULT_RESIDENCY_TIMEOUT
@@ -348,6 +494,14 @@ class ShmLRUCache extends ReaderWriter {
 
     // ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
 
+    /**
+     * Called from within the `set` method.  This will run evictions and return a list of evicted values.
+     * The timeout is applied to an application method given to an exention of this class. 
+     * 
+     * @param {*} age_reduction 
+     * @param {*} e_max 
+     * @returns 
+     */
     immediate_evictions(age_reduction,e_max) {
         let conf = this.conf
         let prev_milisec = (conf.aged_out_secs !== undefined) ? (conf.aged_out_secs*1000) : DEFAULT_RESIDENCY_TIMEOUT
@@ -374,6 +528,13 @@ class ShmLRUCache extends ReaderWriter {
     }
 
 
+    /**
+     * This method calls on the mutex lock. It is not meant to be called within the `set` method.
+     * 
+     * @param {*} age_reduction 
+     * @param {*} e_max 
+     * @returns 
+     */
     immediate_mapped_evictions(age_reduction,e_max) {
         let conf = this.conf
         let prev_milisec = (conf.aged_out_secs !== undefined) ? (conf.aged_out_secs*1000) : DEFAULT_RESIDENCY_TIMEOUT
@@ -396,8 +557,16 @@ class ShmLRUCache extends ReaderWriter {
     }
 
 
+    /**
+     * This method calls on the mutex lock. It is not meant to be called within the `set` method.
+     * 
+     * @param {*} hash_augmented 
+     * @param {*} age_reduction 
+     * @param {*} e_max 
+     * @returns 
+     */
     immediate_targeted_evictions(hash_augmented,age_reduction,e_max) {
-        let pair = hash_augmented.split('-')
+        let pair = Array.isArray(hash_augmented) ?  hash_augmented : hash_augmented.split('-')
 		let hash = parseInt(pair[0])
         let index = parseInt(pair[1])
         if ( this.proc_index < 0 ) return(false)  // guard against bad initialization and test cases
